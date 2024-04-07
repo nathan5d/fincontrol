18cd0111c8ca34c385481b9eeca1c5da5a6ae436
Nathan Amaral <pers.amaral@gmail.com>
2024-04-07 17:18:03 -0400

    fix: pwa

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "fincontrol-cache-v2";
+const CACHE_TIME = 5 * 24 * 60 * 60 * 1000; // 5 dias em milissegundos
+
+const urlsToCache = [
+    "/fincontrol/",
+    "/fincontrol/index.html",
+    "/fincontrol/fallback.html",
+    "/fincontrol/icon.png",
+    // Adicione outros recursos que deseja cachear aqui
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request)
+                .then((networkResponse) => {
+                    if (networkResponse && networkResponse.status === 200) {
+                        const cacheCopy = networkResponse.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, cacheCopy);
+                            });
+                    }
+                    return networkResponse;
+                })
+                .catch(() => {
+                    // Aqui, você pode retornar um fallback do cache ou uma mensagem de erro
+                    return caches.match('fallback.html'); // Você precisa ter um arquivo fallback.html no cache
+                });
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => {
+                        return (
+                            cacheName.startsWith("fincotrol-cache-") &&
+                            cacheName !== CACHE_NAME
+                        );
+                    })
+                    .map((cacheName) => {
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+});
